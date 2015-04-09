f0ee6a73287285a5b4e7da9e236b1fedf447aebe
Chase Replogle <Chase Replogle>
2015-04-08 22:32:02 -0500

    Update Brand Cover

--- classes/class-brand.php
+++ classes/class-brand.php
@@ -23,7 +23,7 @@ function brand_cover($site, $role) { ?>
 
 		$details = get_posts( $args );
 
-		if ($cover_image || $cover_logo) {
+		if ($details) {
 
 			foreach ( $details as $detail ) : ?>
 				<?php $detail_ID = $detail->ID; ?>
@@ -46,6 +46,12 @@ function brand_cover($site, $role) { ?>
 							<img src="<?php echo $cover_image ?>" class="card-image" />
 						</div>
 
+					<?php }  else { ?>
+						<div class="brand-cover brand-cover-logo" style="background-color: #dedede;">
+							<div class="card-inner">
+								<img id="image" src="<?php network_site_url(); ?>/wp-content/themes/brandcards/images/gray.svg"  class="card-image" />
+							</div>
+						</div>
 					<?php } ?>
 				</a>
 			<?php endforeach; ?>
