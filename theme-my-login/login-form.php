8913841d0e50d87ed4a9e49981e7233a5ebcdecf
Chase Replogle <Chase Replogle>
2015-04-09 23:03:29 -0500

    Update

--- theme-my-login/login-form.php
+++ theme-my-login/login-form.php
@@ -3,6 +3,8 @@
 If you would like to edit this file, copy it to your current theme's directory and edit it there.
 Theme My Login will always look in your theme's directory first, before using this default template.
 */
+
+
 ?>
 <div class="login" id="theme-my-login<?php $template->the_instance(); ?>">
 	<?php $template->the_action_template_message( 'login' ); ?>
@@ -38,7 +40,7 @@ Theme My Login will always look in your theme's directory first, before using th
 		</p>
 		<p class="submit">
 			<input type="submit" name="wp-submit" id="wp-submit<?php $template->the_instance(); ?>" value="<?php esc_attr_e( 'Log In' ); ?>" />
-			<input type="hidden" name="redirect_to" value="<?php $_GET['redirect_to']; ?>" />
+			<input type="hidden" name="redirect_to" value="<?php $template->the_redirect_url( 'login' ); ?>" />
 			<input type="hidden" name="instance" value="<?php $template->the_instance(); ?>" />
 			<input type="hidden" name="action" value="login" />
 		</p>
