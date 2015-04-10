a9f9f65dfa572c527e3473bdbde8c17694d644c6
Chase Replogle <Chase Replogle>
2015-04-09 22:31:45 -0500

    Transfer Redirect

--- classes/class-account.php
+++ classes/class-account.php
@@ -407,7 +407,7 @@ function check_user_status_transfer(){
 
     $exists = email_exists($decode_email);
 
-    if ( $exists && pmpro_hasMembershipLevel(array(1,2,3) ) {
+    if ( $exists && pmpro_hasMembershipLevel(array(1,2,3), $id) ) {
        wp_redirect( home_url() . "/login/?transfer_id=$id" ); exit;
     } else {
        wp_redirect( home_url() . "/membership-account/membership-checkout/?level=1&transfer_id=$id" ); exit;
