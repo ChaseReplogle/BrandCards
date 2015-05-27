13808c3ea857a33c62ee26e054fa3b6a327f5e6f
Chase Replogle <Chase Replogle>
2015-05-27 10:53:56 -0500

    Favicon

--- header.php
+++ header.php
@@ -14,6 +14,9 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+
 
 <?php wp_head(); ?>
 </head>
