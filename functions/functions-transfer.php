d65d5cd5b667ad23da3ecfe75b2e80c5ebc96c15
Chase Replogle <Chase Replogle>
2016-03-02 16:08:33 -0600

    Transfer

--- functions/functions-transfer.php
+++ functions/functions-transfer.php
@@ -129,7 +129,7 @@ function transfer_logged_in() {
 
 
 
-	if(isset($_GET['action']) && $_GET['action'] == 'transfer_ownership' && is_user_logged_in() ) {
+	if(isset($_GET['action']) && is_user_logged_in() ) {
 
 		$transfer_id = $_GET['id'];
 		$user_id = get_current_user_id();
