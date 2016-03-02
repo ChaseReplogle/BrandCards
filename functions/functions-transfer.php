195cde5abfc4bc232ef9930e1fe550acff89d2ae
Chase Replogle <Chase Replogle>
2016-03-02 15:32:57 -0600

    Update Login Transfer

--- functions/functions-transfer.php
+++ functions/functions-transfer.php
@@ -118,3 +118,58 @@ add_action('wp_login', 'transfer_login');
 
 
 
+/**
+ * This function hooks into Paid Mebership Pro Checkout:
+ *
+ * @param int    $user_id The ID of the user.
+ *
+ * @return action   Hadles adding invited users to blog upon checkout.
+ */
+function transfer_logged_in($user_login) {
+
+	$transfer_id = $_GET['action'];
+
+	if( $action = 'transfer_ownership' && is_user_logged_in() ) {
+		 $user = wp_get_current_user();
+
+		$transfer_id = $_GET['id'];
+		$user_id = $user->ID;
+		$role = "administrator";
+
+		add_user_to_blog($transfer_id, $user_id, $role);
+
+		$user_info = get_userdata($user_id);
+	    	$transfer_email = $user_info->user_email;
+	    	  global $transfer_email;
+	    	  global $switched;
+			    switch_to_blog($transfer_id);
+			   		$transfers = get_posts( array(
+			   			'post_type'  => 'transfers',
+			   			'meta_key'   => 'transfer_email',
+			   			'meta_value' => $transfer_email
+			   		) );
+			   		foreach ( $transfers as $transfer ) {
+			   			wp_delete_post( $transfer->ID, true);
+			   		}
+
+					$args = array(
+						'blog_id'      => $transfer_id,
+						'role'         => 'administrator',
+				 	);
+
+					$admins = get_users( $args );
+
+					foreach ( $admins as $admin ) {
+						if($admin->ID == $user_id ) {
+							} else {
+								 wp_update_user( array( 'ID' => $admin->ID, 'role' => 'editor' ) );
+							}
+					}
+
+			    restore_current_blog();
+
+	}
+
+}
+add_action('wp', 'transfer_logged_in');
+
