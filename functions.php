82ea6ef7732594e292bc2b3ea1164f7c2dbe364a
Chase Replogle <Chase Replogle>
2016-03-03 10:06:58 -0600

    Style Sheet Cache

--- functions.php
+++ functions.php
@@ -66,7 +66,8 @@ add_filter('show_admin_bar', '__return_false');
  */
 function brandcards_scripts() {
 	wp_enqueue_style( 'brandcards-style', get_stylesheet_uri() );
-	wp_enqueue_style( 'brandcards-style-main', get_template_directory_uri() . '/css/main.css?v=1.3' );
+	$cache_buster = date("YmdHi", filemtime( get_template_directory_uri() . '/css/main.css'));
+	wp_enqueue_style( 'brandcards-style-main', get_template_directory_uri() . '/css/main.css',false, $cache_buster ,'all' );
 	wp_enqueue_style( 'open-sans-font', 'https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' );
 	wp_enqueue_style( 'merriweather-font', 'https://fonts.googleapis.com/css?family=Merriweather:400,300,700' );
 
